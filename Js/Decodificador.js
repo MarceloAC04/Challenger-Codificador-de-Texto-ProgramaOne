b1fa7d5884eaa7925e77b2422ef058c8c179ef68
Marcelo <marcelocardoso020304@gmail.com>
2024-01-24 23:27:40 -0300

    criado as funções de codificar e decodificar

--- Js/Decodificador.js
+++ Js/Decodificador.js
@@ -1,7 +1,52 @@
 function codificadorTexto() {
-    alert("Codificando...!")
+    const text = document.getElementById("input");
+    const character = text.value.split("")
+
+    character.forEach((item, i) => {
+        if(item == "a") {
+            character[i] = "ai";
+
+        } else if(item == "e") {
+            character[i] = "enter";
+
+        } else if(item == "i") {
+            character[i] = "imes";
+
+        } else if(item == "o") {
+            character[i] = "ober";
+
+        } else if(item == "u") {
+            character[i] = "ufat";
+        }
+    });
+    return character.join("");
+}
+
+function escreveCodificado() {
+    const resultado = document.getElementById("show-result")
+    const btnCopiar = document.querySelector(".btn-copy");
+    btnCopiar.classList.remove("btn-invisible");
+    
+    resultado.textContent = codificadorTexto();
 }
 
 function decodificadorTexto() {
-    alert("Decodificando...!")
+    var text = document.getElementById("input").value;
+    const code = [["a", "ai"], ["e", "enter"], ["i", "imes"], ["o", "ober"], ["u", "ufat"]];
+
+    for(var i = 0; i < code.length; i++) {
+        if(text.includes(code[i][1])) {
+            text = text.replaceAll(code[i][1], code[i][0]);
+        }
+    }
+    console.log(text);
+    return text;
+}
+
+function escreveDecodificado() {
+    const resultado = document.getElementById("show-result")
+    const btnCopiar = document.querySelector(".btn-copy");
+    btnCopiar.classList.remove("btn-invisible");
+    
+    resultado.textContent = decodificadorTexto();
 }
